1077d7da002a17ead97380dbe0d6ffde188ed02b
MateuszM <mik.mat66@gmail.com>
2021-07-06 07:57:57 +0200

    MealItemWorking

--- MealAndFitApp/obj/Debug/net5.0-windows/MVVM/View/DayPlanview.g.i.cs
+++ MealAndFitApp/obj/Debug/net5.0-windows/MVVM/View/DayPlanview.g.i.cs
@@ -53,7 +53,7 @@ namespace MealAndFitApp.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MealAndFitApp;V1.0.0.0;component/mvvm/view/dayplanview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MealAndFitApp;component/mvvm/view/dayplanview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\DayPlanView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- MealAndFitApp/obj/Debug/net5.0-windows/MVVM/View/MealView.g.i.cs
+++ MealAndFitApp/obj/Debug/net5.0-windows/MVVM/View/MealView.g.i.cs
@@ -53,7 +53,7 @@ namespace MealAndFitApp.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MealAndFitApp;V1.0.0.0;component/mvvm/view/mealview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MealAndFitApp;component/mvvm/view/mealview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\MealView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
